59086b839808170c89a4b5ded5ea2d4ca87e6b42
Alex Rocha <alex.diego.rocha@gmail.com>
2026-05-06 19:29:41 -0300

    Copiado projeto do Joao Miguel

--- flutter_tennis_turma_a/lib/main.dart
+++ flutter_tennis_turma_a/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tennis/screens/club_list_screen.dart';
-import 'package:flutter_tennis/screens/club_reservation_screen.dart';
+import 'package:flutter_beachup/screens/club_list_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,19 +8,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      scrollBehavior: MaterialScrollBehavior().copyWith(scrollbars: false),
       title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      initialRoute: '/clubs',
-      routes: {
-        '/clubs': (context) => ClubListScreen(),
-        '/clubs/reservation': (context) => ClubReservationScreen(),
-      },
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home:ClubListScreen(),
     );
   }
 }
