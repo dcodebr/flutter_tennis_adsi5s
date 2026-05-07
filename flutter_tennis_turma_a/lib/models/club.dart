34bdd055064fc7f264dcc0e4da000a04093300ec
Alex Rocha <alex.diego.rocha@gmail.com>
2026-05-06 22:25:53 -0300

    Finalização da aula turma A

---
+++ flutter_tennis_turma_a/lib/models/club.dart
@@ -0,0 +1,17 @@
+class Club {
+  String? name;
+  String? location;
+  String? state;
+  String? cover;
+  double? price;
+  String? urlImage;
+
+  Club({
+    required this.name,
+    required this.location,
+    required this.state,
+    required this.cover,
+    required this.price,
+    required this.urlImage,
+  });
+}
--- flutter_tennis_turma_a/lib/screens/club_list_screen.dart
+++ flutter_tennis_turma_a/lib/screens/club_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_beachup/models/club.dart';
 
 class ClubListScreen extends StatefulWidget {
   const ClubListScreen({super.key});
@@ -9,6 +10,36 @@ class ClubListScreen extends StatefulWidget {
 
 class _ClubListScreenState extends State<ClubListScreen> {
   String urlLogo = "https://www.cvtc.com.br/image/beachtennis.jpg";
+  List<Club> clubList = [
+    Club(
+      name: "Quadra Sumaré",
+      location: "Maringá",
+      state: "PR",
+      cover: "Outdoor",
+      price: 19.90,
+      urlImage:
+          "https://images.tcdn.com.br/img/img_prod/1235983/kit_completo_para_quadra_de_beach_tennis_rede_de_protecao_263_1_ea5ab5a7588b6af1553f9c8378cedb09.jpg",
+    ),
+    Club(
+      name: "Quadra Sumaré",
+      location: "Maringá",
+      state: "PR",
+      cover: "Outdoor",
+      price: 19.90,
+      urlImage:
+          "https://images.tcdn.com.br/img/img_prod/1235983/kit_completo_para_quadra_de_beach_tennis_rede_de_protecao_263_1_ea5ab5a7588b6af1553f9c8378cedb09.jpg",
+    ),
+    Club(
+      name: "Quadra Sumaré",
+      location: "Maringá",
+      state: "PR",
+      cover: "Outdoor",
+      price: 19.90,
+      urlImage:
+          "https://images.tcdn.com.br/img/img_prod/1235983/kit_completo_para_quadra_de_beach_tennis_rede_de_protecao_263_1_ea5ab5a7588b6af1553f9c8378cedb09.jpg",
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,14 +53,12 @@ class _ClubListScreenState extends State<ClubListScreen> {
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.only(
                   bottomLeft: Radius.circular(70),
-                  bottomRight: Radius.circular(70)
+                  bottomRight: Radius.circular(70),
                 ),
               ),
               width: double.infinity,
-              child: Image.network(
-                urlLogo, fit: BoxFit.cover,
-              )
-              ),
+              child: Image.network(urlLogo, fit: BoxFit.cover),
+            ),
           ),
           Expanded(
             flex: 1,
@@ -42,17 +71,22 @@ class _ClubListScreenState extends State<ClubListScreen> {
                     child: Container(
                       decoration: BoxDecoration(
                         color: Colors.grey,
-                        borderRadius: BorderRadius.circular(50)
+                        borderRadius: BorderRadius.circular(50),
                       ),
                       child: Row(
                         children: [
+                          Expanded(flex: 1, child: Icon(Icons.search)),
                           Expanded(
-                            flex:1, 
-                            child: Icon (Icons.search)
+                            flex: 9,
+                            child: Padding(
+                              padding: const EdgeInsets.only(bottom: 20),
+                              child: TextField(
+                                decoration: InputDecoration(
+                                  hintText: "Search",
+                                  border: InputBorder.none,
+                                ),
+                              ),
                             ),
-                          Expanded(
-                          flex: 1,
-                          child: TextField()
                           ),
                         ],
                       ),
@@ -65,16 +99,37 @@ class _ClubListScreenState extends State<ClubListScreen> {
                       backgroundColor: const Color.fromARGB(255, 30, 62, 31),
                       foregroundColor: Colors.yellow,
                       child: Icon(Icons.tune),
-                      onPressed: (){}
-                    )
+                      onPressed: () {},
                     ),
+                  ),
                 ],
               ),
             ),
           ),
           Expanded(
             flex: 6,
-            child: Placeholder(),
+            child: SingleChildScrollView(
+              child: Column(
+                children: clubList
+                    .map(
+                      (club) => Container(
+                        clipBehavior: Clip.antiAlias,
+                        height: 165,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25),
+                          color: Colors.blue,
+                        ),
+                        child: Row(
+                          children: [
+                            Expanded(flex: 5, child: Placeholder()),
+                            Expanded(flex: 4, child: Placeholder()),
+                          ],
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
           ),
         ],
       ),
