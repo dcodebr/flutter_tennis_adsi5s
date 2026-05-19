3fb1a670c181b8976ab6f5dd83adc02eea83c567
alex.diego.rocha@gmail.com <alex.diego.rocha@gmail.com>
2026-05-19 20:48:25 -0300

    Add clubscontroller

--- flutter_tennis_turma_b/lib/services/clubs_service.dart
+++ flutter_tennis_turma_b/lib/services/clubs_service.dart
@@ -1,1 +1,8 @@
-class ClubController {}
+import 'package:flutter_beachup/models/club.dart';
+
+class ClubsService {
+  final String baseUrl =
+      "https://mocki.io/v1/2ad1cb8b-67b9-4fbb-84bf-9dc25201f6b9";
+
+  Future<List<Club>> getClubs() async {}
+}
